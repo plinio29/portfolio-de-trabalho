4ade1d3a30b721336007a38d5ca95da61ca090eb
Plínio Ramos <plinio206@live.com>
2023-01-27 22:50:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portifolio-de-trabalho
+ O projeto foi desenvolvido com o objetivo de apresentar meus trabalhos de maneira simples e eficiente, nele foi utilizada as tecnologias HTML, CSS e JavaScript.
---
+++ script/imagens.js
@@ -0,0 +1,78 @@
+var botao1 = document.getElementById('botao1');
+var botao2 = document.getElementById('botao2');
+var botao3 = document.getElementById('botao3');
+var botao4 = document.getElementById('botao4');
+var img = document.getElementById('img');
+var titulo = document.getElementById('titulo-proj')
+var descricao = document.getElementById('descri-proj')
+var visita = document.getElementById('visita')
+
+botao1.addEventListener('click', alteraimg1);
+botao2.addEventListener('click', alteraimg2);
+botao3.addEventListener('click', alteraimg3);
+botao4.addEventListener('click', alteraimg4);
+
+function alteraimg1(){
+    img.src = 'imagens/proje1.jpg'
+    titulo.innerText = 'Portifólio de trabalho'
+    descricao.innerText = 'O projeto foi desenvolvido com o objetivo de apresentar meus trabalhos de maneira simples e eficiente, nele foi utilizada as tecnologias HTML, CSS e JavaScript.'
+    botao1.style.backgroundColor = '#407BFF'
+    botao1.style.color = '#ffff'
+
+    botao2.style.backgroundColor = '#ffff'
+    botao2.style.color = '#407BFF'
+    botao3.style.backgroundColor = '#ffff'
+    botao3.style.color = '#407BFF'
+    botao4.style.backgroundColor = '#ffff'
+    botao4.style.color = '#407BFF'
+
+    visita.href = 'https://github.com/plinio29/layout-desafio-mandatrends'
+}
+function alteraimg2(){
+    img.src = 'imagens/proje2.jpg'
+    titulo.innerText = 'Desafio - MANDATRENDS'
+    descricao.innerText = 'O desafio proposto pela empresa MANDARIN era criar um layout em HTML totalmente responsivo, no projeto foram utilizadas as tecnologias HTML e CSS.'
+    botao2.style.backgroundColor = '#407BFF'
+    botao2.style.color = '#ffff'
+
+    botao1.style.backgroundColor = '#ffff'
+    botao1.style.color = '#407BFF'
+    botao3.style.backgroundColor = '#ffff'
+    botao3.style.color = '#407BFF'
+    botao4.style.backgroundColor = '#ffff'
+    botao4.style.color = '#407BFF'
+
+    visita.href = 'https://github.com/plinio29/layout-desafio-mandatrends'
+}
+function alteraimg3(){
+    img.src = 'imagens/proje3.jpg'
+    titulo.innerText = 'Desafio - Curso em Vídeo'
+    descricao.innerText = 'O desafio proposto no curso de HTML e CSS era de replicar a página web, no projeto foram utilizadas as tecnologias HTML e CSS.'
+    botao3.style.backgroundColor = '#407BFF'
+    botao3.style.color = '#ffff'
+
+    botao1.style.backgroundColor = '#ffff'
+    botao1.style.color = '#407BFF'
+    botao2.style.backgroundColor = '#ffff'
+    botao2.style.color = '#407BFF'
+    botao4.style.backgroundColor = '#ffff'
+    botao4.style.color = '#407BFF'
+
+    visita.href = 'https://github.com/plinio29/site-android-desafio'
+}
+function alteraimg4(){
+    img.src = 'imagens/proje4.jpg'
+    titulo.innerText = 'Sistema de cadastro de clientes'
+    descricao.innerText = 'O projeto consiste em um sistema em pythom que permite adicionar, pesquisr, editar e excluir dados de clientes, no projeto foram utilizadas as tecnologias Python, PyQT5, QT designer e arquivo de texto.'
+    botao4.style.backgroundColor = '#407BFF'
+    botao4.style.color = '#ffff'
+
+    botao1.style.backgroundColor = '#ffff'
+    botao1.style.color = '#407BFF'
+    botao2.style.backgroundColor = '#ffff'
+    botao2.style.color = '#407BFF'
+    botao3.style.backgroundColor = '#ffff'
+    botao3.style.color = '#407BFF'
+
+    visita.href = 'https://github.com/plinio29/sistema-de-cadastro-python'
+}
